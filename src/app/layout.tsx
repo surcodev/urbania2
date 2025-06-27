3a601dba710b9b00695f8d7ee10a3a69725abc63
surcodev <temporal98x@gmail.com>
2025-06-27 12:17:29 -0500

    cabecera pero con padding y cabecera mantiene en sig-in y sign-up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,32 +10,45 @@ import {
   SignedOut,
   UserButton
 } from '@clerk/nextjs'
+import { esES } from '@clerk/localizations'
+import { currentUser } from '@clerk/nextjs/server'
 
 export const metadata: Metadata = {
   title: "Venta de departamentos, casas y terrenos",
   description: "Una sola parada para todas sus necesidades inmobiliarias",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedInUser = await currentUser();
+  let username = loggedInUser?.username
+  if (!username) {
+    username = loggedInUser?.firstName + " " + loggedInUser?.lastName;
+  }
+  username = username.replace("null", "")
+
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={esES}>
       <html lang="es">
         <body>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
+          <header className="flex justify-end items-center p-4 gap-4 h-3 bg-[#1b4242]">
             <SignedOut>
               <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+              <SignUpButton >
+                <button className="">
                   Sign Up
                 </button>
               </SignUpButton>
             </SignedOut>
             <SignedIn>
-              <UserButton />
+              <UserButton signInUrl="/sign-in" />
+              <h1>Clerk user id : {loggedInUser?.id}</h1>
+              <h1>Username : {username}</h1>
+              <h1>Email : {loggedInUser?.emailAddresses[0].emailAddress}</h1>
             </SignedIn>
           </header>
           <ThemeProvider>{children}</ThemeProvider>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#1b4242'
       },
     },
   },
